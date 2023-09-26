fcc1b7659fbdfdfe4efd36cf8b6eef733e3b73ec
Awzel <134027924+Awzel@users.noreply.github.com>
2023-09-26 18:38:26 -0400

    Update EvenOrOdd_Awzel.java

--- src/Week01_Tasks/EvenOrOdd_Awzel.java
+++ src/Week01_Tasks/EvenOrOdd_Awzel.java
@@ -14,9 +14,9 @@ public class EvenOrOdd_Awzel {
 
     static void oddOrEven(int number) {
         if (number % 2 == 0){
-            System.out.println(number + " is an odd number");
-        } else {
             System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
         }
     }
 
