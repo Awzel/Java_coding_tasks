08c624208fe5ab3b3811bf1989f386a2f54fb4e2
Awzel <134027924+Awzel@users.noreply.github.com>
2023-09-28 10:48:23 -0400

    Update FINRA_Awzel.java

--- src/Week01_Tasks/FINRA_Awzel.java
+++ src/Week01_Tasks/FINRA_Awzel.java
@@ -15,9 +15,9 @@ public class FINRA_Awzel {
             if (i % 3 == 0 && i % 5 ==0){
                 System.out.println("FINRA");
             }else if (i % 3 == 0){
-                System.out.println("RA");
-            }else if (i % 5 == 0) {
                 System.out.println("FIN");
+            }else if (i % 5 == 0) {
+                System.out.println("RA");
             }else {
                 System.out.println(i);
             }
