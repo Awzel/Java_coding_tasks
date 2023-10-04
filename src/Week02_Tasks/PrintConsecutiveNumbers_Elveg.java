49882c67ac6ae5d149a1a90c4765a05981998119
elvegl <v.cutied@gmail.com>
2023-10-04 18:23:49 -0400

    Solved week 2 tasks

---
+++ src/Week02_Tasks/PrintConsecutiveNumbers_Elveg.java
@@ -0,0 +1,21 @@
+package Week02_Tasks;
+
+public class PrintConsecutiveNumbers_Elveg {
+    public static void main(String[] args) {
+        String str ="";
+        int N = 100;
+        for(int i = 1; i<N; i++){
+            if(i%2==0){
+                str+="Codility";
+            }if (i%3==0) {
+                str+="Test";
+            }if (i%5==0) {
+                str+="Coders";
+            }if(!((i%2==0)||(i%3==0)||(i%5==0))){
+            str+=i;}
+            System.out.println(str);
+            str="";
+            }
+        }
+    }
+
