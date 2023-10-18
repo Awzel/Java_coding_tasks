92bb31ef30251c3df9013637c8438d67454358ef
sabinasattarova <sabiwasf@gmail.com>
2023-10-18 11:42:42 -0400

    week4_tasks classes

---
+++ src/Week04_Tasks/FrequencyOfChar_Sabina.java
@@ -0,0 +1,14 @@
+package Week04_Tasks;
+
+public class FrequencyOfChar_Sabina {
+    //String -- Frequency of Characters
+    //Write a return method that can find the frequency of
+    //characters
+    //Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+
+
+
+
+
+
+}
---
+++ src/Week04_Tasks/RemoveDuplicates_Sabina.java
@@ -0,0 +1,9 @@
+package Week04_Tasks;
+
+public class RemoveDuplicates_Sabina {
+
+    //String -- Remove Duplicates
+    //Write a return method that can remove the duplicated values from
+    //String
+    //Ex: removeDup("AAABBBCCC") ==> ABC
+}
