76698e1fbbaab826689a9eace3d0a3819df12573
wardahafeez <hafeez.warda@gmail.com>
2023-10-19 09:44:39 -0400

    Created new DivideWithoutOperator

---
+++ src/Week02_Tasks/RemoveDuplicates_Warda.java
@@ -0,0 +1,22 @@
+package src.Week02_Tasks;
+
+public class RemoveDuplicates_Warda {
+    public static void main(String[] args) {
+        String str = "AAABBCDD";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char eachletter = str.charAt(i);
+            if (result.contains("" + eachletter)) {
+                continue;
+            } else {
+                result += eachletter;
+
+            }
+        }
+
+        System.out.println(result);
+
+
+    }
+}
+
