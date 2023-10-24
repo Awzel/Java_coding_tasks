16cb4e4bdaa71f1ca64c05eae6227d6e890529f0
wardahafeez <hafeez.warda@gmail.com>
2023-10-24 19:10:54 -0400

    Updated from develop to warda branch

--- src/Week01_Tasks/DividewithoutOperator_Warda.java
+++ src/Week01_Tasks/DividewithoutOperator_Warda.java
@@ -1,23 +1,55 @@
-package Week01_Tasks;
+package src.Week01_Tasks;
 
-public class DividewithoutOperator_Warda {
+public class DividewithoutOPerator_Warda {
     public static void main(String[] args) {
-        int n1 = 15;
-        int n2 = 5;
+        int dividend = 25;
+        int divisor = 2;
         int count=0;
 
-        while (n1>n2||n1==n2){
+        while (dividend>divisor||dividend==divisor){
             count++;
-            n1=n1-n2;
+            dividend=dividend-divisor;
 
 
         }
 
-        System.out.println(count+" with a remainder of "+(n1));
+        System.out.println(count);
 
     }
-}
+
 /*2. Numbers -- Divide without/operator
 
 Write a method that can divide two numbers without using division operator*/
 
+/* COMPREHENSIVE ANSWER
+
+System.out.println(divide(-50,5));
+
+    }
+
+    public static int divide(int dividend, int divisor){
+
+        // first handle divisibility by 0
+        if (divisor == 0){
+            throw new ArithmeticException("Can't be divided by 0");
+        }
+
+        // we have to handle negative numbers since there won't be any ending to this
+        // for example -1 -1 = -2. -2 -1 = -3, it will go to negative infinity
+
+        if (dividend < 0 ){
+            dividend = - dividend;
+        }
+        if (divisor < 0 ){
+            divisor = - divisor;
+        }
+
+        // perform the division using repeated subtraction
+        int quotient = 0;
+        while (dividend >= divisor){
+            dividend -= divisor;
+            quotient++;
+        }
+
+        return quotient;*/
+}
